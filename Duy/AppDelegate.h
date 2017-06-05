80cdee19abd0715dfb792aa10f77323b451561bd
Do Ngoc Duy <writon1@gmail.com>
2017-06-05 14:05:33 +0700

    commit calculator_1

---
+++ Duy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by dankhanh on 02/06/2017.
+//  Copyright © 2017 duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
