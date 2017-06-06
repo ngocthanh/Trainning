10126ae2ffed8dbf4543129b0ae12f1513a0c020
Do Ngoc Duy <writon1@gmail.com>
2017-06-06 09:45:47 +0700

    Update source-1

--- Duy/Calculator/Calculator/ViewController.m
+++ Duy/Calculator/Calculator/ViewController.m
@@ -10,220 +10,132 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *lblDisplay;
 @property (weak, nonatomic) NSString *storage;
-@property (weak, nonatomic) NSString *temp;
 @property (weak, nonatomic) NSString *operation;
 
-@property (nonatomic) Boolean IsfirstInput;
-@property (nonatomic) Boolean IsfirstOperation;
+@property (nonatomic) Boolean userIsInputingNumber;
 @end
 
 @implementation ViewController
+NSString *MyDot=@".";
+float _temp;
+//int _time;
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _lblDisplay.text=[NSString stringWithFormat:@""];
+    _lblDisplay.text=[NSString stringWithFormat:@"0"];
     _storage=@"0";
-    _IsfirstInput=true;
-    _IsfirstOperation=false;
 }
 - (IBAction)btnNum0Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-        //_IsfirstOperation=true;
-    }
+    [self AppendNumber:@"0"];
 }
 - (IBAction)btnNum1Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-       // _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@1",_lblDisplay.text];
+    [self AppendNumber:@"1"];
 }
 - (IBAction)btnNum2Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-      //  _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@2",_lblDisplay.text];
+    [self AppendNumber:@"2"];
 }
 - (IBAction)btnNum3Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-       // _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@3",_lblDisplay.text];
+    [self AppendNumber:@"3"];
 }
 - (IBAction)btnNum4Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-       // _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@4",_lblDisplay.text];
+    [self AppendNumber:@"4"];
 }
 - (IBAction)btnNum5Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-//_IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@5",_lblDisplay.text];
+    [self AppendNumber:@"5"];
 }
 - (IBAction)btnNum6Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-       // _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@6",_lblDisplay.text];
+    [self AppendNumber:@"6"];
 }
 - (IBAction)btnNum7Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-      //  _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@7",_lblDisplay.text];
+    [self AppendNumber:@"7"];
 }
 - (IBAction)btnNum8Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-      //  _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@8",_lblDisplay.text];
+    [self AppendNumber:@"8"];
 }
 - (IBAction)btnNum9Clicked:(UIButton *)sender {
-    if (!_IsfirstInput || _IsfirstOperation)
-    {
-        _lblDisplay.text=@"";
-        _IsfirstInput=true;
-      //  _IsfirstOperation=true;
-    }
-    _lblDisplay.text=[NSString stringWithFormat:@"%@9",_lblDisplay.text];
+    [self AppendNumber:@"9"];
 }
 - (IBAction)btnCommaClicked:(UIButton *)sender {
-    _operation=@"ADD";
+    NSString *currentText = _lblDisplay.text;
+    if ([currentText rangeOfString:@"." options:NSBackwardsSearch].length == 0) {
+        [self AppendNumber:@"."];
+    }
 }
 - (IBAction)btnDivideClicked:(UIButton *)sender {
-    if(_IsfirstOperation){
-        _temp = _lblDisplay.text;
-        _storage=_temp;
-        _lblDisplay.text=@"";
-        _IsfirstOperation=false;
-    }
-    else{
-        _temp = _lblDisplay.text;
-        [self calculate:_storage:_temp];
-        _lblDisplay.text=_storage;
-        _IsfirstOperation=false;
-        _IsfirstInput=false;
-    }
+    _temp=[[self DisplayValue] floatValue];
     _operation=@"DIVIDE";
+    _userIsInputingNumber = true;
 }
 - (IBAction)btnMultiplyClicked:(UIButton *)sender {
-    if(_IsfirstOperation){
-        _temp = _lblDisplay.text;
-        _storage=_temp;
-        _lblDisplay.text=@"";
-        _IsfirstOperation=false;
-    }
-    else{
-        _temp = _lblDisplay.text;
-        [self calculate:_storage:_temp];
-        _lblDisplay.text=_storage;
-        _IsfirstOperation=false;
-        _IsfirstInput=false;
-    }
+    _temp=[[self DisplayValue] floatValue];
     _operation=@"MULTIPLY";
+    _userIsInputingNumber = true;
 }
 - (IBAction)btnMinusClicked:(UIButton *)sender {
-    
-    if(_IsfirstOperation){
-        _temp = _lblDisplay.text;
-        _storage=_temp;
-        _lblDisplay.text=@"";
-        _IsfirstOperation=false;
-    }
-    else{
-        _temp = _lblDisplay.text;
-        [self calculate:_storage:_temp];
-        _lblDisplay.text=_storage;
-        _IsfirstOperation=false;
-        _IsfirstInput=false;
-    }
+    _temp=[[self DisplayValue] floatValue];
     _operation=@"MINUS";
+    _userIsInputingNumber = true;
 }
 - (IBAction)btnAddClicked:(UIButton *)sender {
-    
-    if(_IsfirstOperation){
-        _temp = _lblDisplay.text;
-        _storage=_temp;
-        _lblDisplay.text=@"";
-        _IsfirstOperation=false;
-        }
-    else{
-        _temp = _lblDisplay.text;
-        [self calculate:_storage:_temp];
-        _lblDisplay.text=_storage;
-        _IsfirstOperation=false;
-        _IsfirstInput=false;
-        }
+    _temp=[[self DisplayValue] floatValue];
     _operation=@"ADD";
+    _userIsInputingNumber = true;
     }
--(void)calculate:(NSString *)FirstNumber :(NSString *)SecondNumber{
-    if ([_operation isEqual:@"ADD"]) {
-        _storage=[NSString stringWithFormat:@"%f",[FirstNumber doubleValue]+[SecondNumber doubleValue]];
-    }
-    if ([_operation isEqual:@"MINUS"]) {
-        _storage=[NSString stringWithFormat:@"%f",[FirstNumber doubleValue]-[SecondNumber doubleValue]];
-    }
-    if ([_operation isEqual:@"MULTIPLY"]) {
-        _storage=[NSString stringWithFormat:@"%f",[FirstNumber doubleValue]*[SecondNumber doubleValue]];
-    }
-    if ([_operation isEqual:@"DIVIDE"]) {
-        _storage=[NSString stringWithFormat:@"%f",[FirstNumber doubleValue]/[SecondNumber doubleValue]];
-    }
-}
-
-- (IBAction)btnEqualClicked:(UIButton *)sender {
-    if(!_IsfirstOperation){
-        _storage=_lblDisplay.text;
-        [self calculate:_temp : _storage];
-        _lblDisplay.text=[NSString stringWithFormat:@"%@",_storage];
-            }
+-(void)calculate{
+    //_time=[self numberOfTimesBtnEqualClicked];
+    
+    float running=[[self DisplayValue] floatValue];
+    _storage=_lblDisplay.text;
+    if(_temp==0)
+        _storage=[NSString stringWithFormat:@"%f",_temp];
     else
-        {
-            [self calculate:_storage : _lblDisplay.text];
-            _lblDisplay.text=[NSString stringWithFormat:@"%@",_storage];
+        {if ([_operation isEqual:@"ADD"]) {
+            _storage=[NSString stringWithFormat:@"%f",_temp+running];
+        }else if ([_operation isEqual:@"MINUS"]) {
+            _storage=[NSString stringWithFormat:@"%f",_temp-running];
+        }else if ([_operation isEqual:@"MULTIPLY"]) {
+            _storage=[NSString stringWithFormat:@"%f",_temp*running];
+        }else if([_operation isEqual:@"DIVIDE"]) {
+            _storage=[NSString stringWithFormat:@"%f",(running ==0.0)?0.0:_temp/running];
+            }
         }
-    _IsfirstOperation=true;
+}
+- (IBAction)btnEqualClicked:(UIButton *)sender {
+    [self calculate];
+    _lblDisplay.text=[NSString stringWithFormat:@"%@",_storage];
+    _operation=@"";
+    _userIsInputingNumber=true;
+    //_time+=1;
 }
 - (IBAction)btnClearClicked:(UIButton *)sender {
-    _lblDisplay.text=[NSString stringWithFormat:@""];
-    _storage=@"0";
-    _IsfirstInput=false;
-    _IsfirstOperation=true;
+    _storage=0;
+    _lblDisplay.text=@"0";
+    _operation=@"";
+    _userIsInputingNumber=false;
+//    _time=0;
 }
-
+- (void)AppendNumber:(NSString *)Number{
+    if ([_lblDisplay.text isEqualToString:@"0"] && ![Number isEqualToString:@"."]) {
+        _lblDisplay.text=Number;
+    }else if (_userIsInputingNumber){
+            _lblDisplay.text=Number;
+            _userIsInputingNumber=false;
+    }else{
+        _lblDisplay.text=[_lblDisplay.text stringByAppendingString:Number];
+    }
+}
+- (NSNumber *)DisplayValue{
+    NSNumberFormatter *f = [[NSNumberFormatter alloc] init];
+    [f setNumberStyle:NSNumberFormatterDecimalStyle];
+    return [f numberFromString:self.lblDisplay.text];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-
+//-(int)numberOfTimesBtnEqualClicked{
+//    return _time;
+//}
 @end
