35185f81d2805ba7d2d463d52abcdecb60e4557f
KterKC <kterquyen@gmail.com>
2017-06-26 11:56:05 +0700

    Update Create File

---
+++ FacebookSDK/Source Code/Photo Facebook/Photo Facebook/Support/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo Facebook
+//
+//  Created by KterKC on 6/24/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacebookSDK/Source Code/Photo Facebook/Photo Facebook/ViewController/AccountInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountInformationViewController.h
+//  Photo Facebook
+//
+//  Created by KterKC on 6/26/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountInformationViewController : UIViewController
+
+@end
