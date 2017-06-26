2801fd3d4eef4d6a515bf3139afbd281164a5323
KterKC <kterquyen@gmail.com>
2017-06-26 12:04:51 +0700

    Update Create File

---
+++ FacebookSDK/Source Code/Photo Facebook/Photo Facebook/Support/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Photo Facebook
+//
+//  Created by KterKC on 6/24/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FacebookSDK/Source Code/Photo Facebook/Photo Facebook/Utility/ConstantsSystem.h
@@ -0,0 +1,13 @@
+//
+//  ConstantsSystem.h
+//  Photo Facebook
+//
+//  Created by KterKC on 6/26/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConstantsSystem : NSObject
+
+@end
