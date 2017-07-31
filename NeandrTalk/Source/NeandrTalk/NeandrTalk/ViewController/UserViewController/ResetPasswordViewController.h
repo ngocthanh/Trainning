ca5c0e4a9cd64bbcec677063bbd51610bee19d24
Do Ngoc Duy <writon1@gmail.com>
2017-07-31 14:29:17 +0700

    Commit reset password

---
+++ NeandrTalk/Source/NeandrTalk/NeandrTalk/ViewController/UserViewController/ResetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResetPasswordViewController.h
+//  NeandrTalk
+//
+//  Created by dankhanh on 7/31/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResetPasswordViewController : UIViewController
+
+@end
---
+++ NeandrTalk/Source/NeandrTalk/NeandrTalk/ViewController/UserViewController/ResetPasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResetPasswordViewController.m
+//  NeandrTalk
+//
+//  Created by dankhanh on 7/31/17.
+//  Copyright © 2017 KterKC. All rights reserved.
+//
+
+#import "ResetPasswordViewController.h"
+
+@interface ResetPasswordViewController ()
+
+@end
+
+@implementation ResetPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationController setNavigationBarHidden:NO];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
